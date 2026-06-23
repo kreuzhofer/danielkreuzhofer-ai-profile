0617b415a9f73412e0889f3a372175cb8e9fd4e6
Daniel Kreuzhofer <1763467+kreuzhofer@users.noreply.github.com>
2026-06-23 14:42:03 +0200

    feat(scorecard): public barrel for the engine core

---
+++ frontend/src/lib/scorecard/index.ts
@@ -0,0 +1,6 @@
+export * from "./types";
+export { computeRawSum, normalizeScore, computeCategoryScores } from "./scoring";
+export { resolveOutcome, type OutcomeContext } from "./outcome";
+export { isQualified } from "./qualification";
+export { computeNextLever } from "./next-lever";
+export { buildResult } from "./result";
